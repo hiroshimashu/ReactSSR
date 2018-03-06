e35e3c5da9b098021f0ae932756418e4b149494e
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-03-06 16:09:20 +0900

    add addging Navigation' '

---
+++ src/helpers/render.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import Routes from '../client/Routes';
+
+export default (req) => {
+    const content = renderToString(
+        <StaticRouter location = {req.url} context = {{}}>
+            <Routes />
+        </StaticRouter>
+    );
+
+    return `
+        <html>
+            <head>
+            <body>
+            <div id = 'root'>
+                ${content}
+            </div>
+            <script src = "bundle.js"></script></script>
+            </body>
+            </head>
+        </html>
+        
+    `;
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from './helpers/render';
 
 const app = express();
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-    const content = renderToString(<Home />);
-
-    const html = `
-        <html>
-            <head>
-            <body>
-            <div id = 'root'>
-                ${content}
-            </div>
-            <script src = "bundle.js"></script></script>
-            </body>
-            </head>
-        </html>
-        
-    `;
-    res.send(content);
+    res.send(renderer(req));
 });
 
 app.listen(3000, () => {
