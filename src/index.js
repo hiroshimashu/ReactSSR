8d1d5ff30ae5ca1c5de1bf750d0ac74e80f53853
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-03-09 20:47:49 +0900

    add adding reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+//https://react-ssr-api.herokuapp.com
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/render';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.send(renderer(req));
+app.get('*', (req, res) => {
+    const store = createStore();
+
+
+    res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
